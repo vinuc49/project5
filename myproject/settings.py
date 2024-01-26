3aa40d345ccf5461e3b960397cb87fa4e62d3e35
vinuc49 <vinuchandregowda@gmail.com>
2024-01-26 21:15:45 +0000

    1

--- myproject/settings.py
+++ myproject/settings.py
@@ -32,6 +32,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
+X_FRAME_OPTIONS = 'SAMEORIGIN'
+
 ALLOWED_HOSTS = [".herokuapp.com", "localhost", "127.0.0.1", "project5-food-eff6062cfe83.herokuapp.com"]
 
 
